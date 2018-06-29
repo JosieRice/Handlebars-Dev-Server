fe1e2043693e434db37e682e67e09d7da99b337f
unknown <craigricetong@gmail.com>
2018-06-29 14:22:38 -0700

    express server working on test hbs files

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
 const app = express();
 
+// Register Handlebars view engine
+app.engine('hbs', exphbs());
+// Use Handlebars view engine
+app.set('view engine', 'hbs');
+
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.render('templates/widget');
 });
 
 app.listen(3000, () => {
